15b9ed7dded8df46c3ae1a771a30f0b98e81c51e
ryan <ry0n4.irv@gmail.com>
2025-01-24 02:50:32 +0000

    I added a test method to BetterBasket.

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -13,8 +13,8 @@ public class BetterBasket extends Basket implements Serializable
 {
   private static final long serialVersionUID = 1L;
   
+  public void testMethod() {
+    System.out.println("Ignore ME<3");
+  }
 
-  // You need to add code here
-  // merge the items for same product,
-  // or sort the item based on the product number
 }
